18bf285064c5de9d5a6070ff9a6a213a5c438fb8
Shrikant Wadkar <100507781+ShrikantWadkar14@users.noreply.github.com>
2025-06-20 23:38:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -157,3 +157,6 @@ def api_predict():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=10000)
